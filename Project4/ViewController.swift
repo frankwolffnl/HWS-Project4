55d818e5ede903d0745f8c5a8930578738123abf
frankwolffnl <frankwolff.nl@gmail.com>
2017-11-02 16:13:19 +0100

    Added progress bar when loading web page.

--- Project4/ViewController.swift
+++ Project4/ViewController.swift
@@ -11,6 +11,7 @@ import WebKit
 
 class ViewController: UIViewController, WKNavigationDelegate {
 	var webView: WKWebView!
+	var progressView: UIProgressView!
 	
 	override func loadView() {
 		webView = WKWebView()
@@ -22,10 +23,21 @@ class ViewController: UIViewController, WKNavigationDelegate {
 		super.viewDidLoad()
 		
 		navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+		progressView = UIProgressView(progressViewStyle: .default)
+		progressView.sizeToFit()
+		let progressButton = UIBarButtonItem(customView: progressView)
+		
+		let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+		let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+		
+		toolbarItems = [progressButton, spacer, refresh]
+		navigationController?.isToolbarHidden = false
 		
 		let url = URL(string: "https://www.hackingwithswift.com")!
 		webView.load(URLRequest(url: url))
 		webView.allowsBackForwardNavigationGestures = true
+		
+		webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
 	}
 	
 	@objc func openTapped() {
@@ -44,6 +56,12 @@ class ViewController: UIViewController, WKNavigationDelegate {
 	
 	func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
 		title = webView.title
+	}
+	
+	override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+		if keyPath == "estimatedProgress" {
+			progressView.progress = Float(webView.estimatedProgress)
+		}
 	}
 
 	override func didReceiveMemoryWarning() {
